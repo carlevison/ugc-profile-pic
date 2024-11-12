e89437b958e04439a42cb4c1bfacffed2b98fa6e
Caroline Levison <caroline.levison@cloudinary.com>
2024-11-12 15:00:48 +0000

    Set cloud name again

--- app/config/cloudinary.ts
+++ app/config/cloudinary.ts
@@ -1,5 +1,5 @@
 export const CLOUDINARY_CONFIG = {
-    cloudName: 'MY_CLOUD_NAME',
+    cloudName: 'cld-demo-ugc',
     uploadPreset: 'ugc-profile-photo',
     defaultImage: 'avatar-pic'
   } as const
